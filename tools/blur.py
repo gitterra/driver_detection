485fba740d27a62e07a6153383dc933291ed0cf0
gitterra <80519167+gitterra@users.noreply.github.com>
2023-03-03 21:52:53 +0300

    Create blur.py

---
+++ tools/blur.py
@@ -0,0 +1,28 @@
+import os
+import re
+from . import bcolors
+
+class Worker:
+    def __init__(self):
+        self.index_class = 3
+
+    def analysis(self, directory):
+        print(f'{bcolors.OKBLUE}Этап: {bcolors.BOLD}РАЗМЫТОЕ ИЗОБРАЖЕНИЕ', end='')
+        try:
+            result = []
+            if os.path.isdir(directory):
+                for f in sorted(os.listdir(directory)):
+                    with open(f'{directory}/{f}', 'r') as fl:
+                        data = fl.read().split()
+                    belt_exist = False
+                    for i in range(0, len(data), 5):
+                        if int(data[i]) == self.index_class:
+                            belt_exist = True
+                            break
+                    if not belt_exist:
+                        result.append(int(re.findall('[0-9]+', f)[0]))
+            print(f'{bcolors.ENDC}{bcolors.OKGREEN} Done{bcolors.ENDC}')
+            return sorted(result)
+        except:
+            print(f'{bcolors.ENDC}{bcolors.FAIL} Done{bcolors.ENDC}')
+            return []
